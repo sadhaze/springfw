ebd8052527357101e0c107b16822c63d86e86ef0
Litvinenko-AS <aslitvinenko@sberbank.ru>
2018-12-12 17:45:01 +0300

    Commit 4

--- src/main/java/ru/edu/otus/springfw/config/WebConfig.java
+++ src/main/java/ru/edu/otus/springfw/config/WebConfig.java
@@ -3,7 +3,6 @@ package ru.edu.otus.springfw.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
